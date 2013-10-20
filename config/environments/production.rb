17c121fd7039033ce503a6cf33508bf527f347db
Emmanuel Turlay <bluesmanu@hotmail.com>
2013-10-20 15:21:37 +0200

    fix s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Thebigshake::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  Paperclip.interpolates(:s3_eu_url) { |attachment, style|
+    "#{attachment.s3_protocol}://s3-eu-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
+  }
 end
