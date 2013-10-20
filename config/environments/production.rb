5f292be44aa5309d979e9ad51a4468f655112c9c
Emmanuel Turlay <bluesmanu@hotmail.com>
2013-10-20 15:14:57 +0200

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Thebigshake::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.default_url_options = { :host => 'thebigshake.herokuapp.com' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
